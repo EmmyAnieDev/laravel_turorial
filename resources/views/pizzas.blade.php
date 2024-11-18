65d0b2c6fcf7c8134239a0c85c29a6b45cdbe6f3
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-11-18 19:22:56 +0100

    Learnt about Blade basics @ symbol if statement unless statement and php directive

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -15,10 +15,33 @@
         <div class="flex-center position-ref full-height">
 
            <div class="content">
-            <div class="title m-b-md">
-                Pizza List
-            </div>
-            <p>{{ $type }} - {{ $base }} - {{ $price }}</p>
+                <div class="title m-b-md">
+                    Pizza List
+                </div>
+                <h1><?php echo(htmlspecialchars($type)) ?> - <?php echo(htmlspecialchars($base)) ?> - <?php echo(htmlspecialchars($price)) ?> </h1>  <!-- using regular php -->
+                <h1>{{ $type }} - {{ $base }} - {{ $price }}</h1>  <!-- using blade syntax -->
+
+                <!-- In Laravel's Blade templating engine, the @ symbol is used to indicate a Blade directive.  -->
+                @if($price > 15)
+                    <p>The Pizza is expensive</p>
+                @elseif($price < 5)
+                    <p>The Pizza is cheap</p>
+                @else
+                    <p>This pizza is normall priced</p>
+                @endif
+
+                <!-- This only output if it's false. (somehow like opposite of if statement) -->
+                @unless($base == 'chessy crust')
+                    <p>you don't have a Chessy crust</p>
+                @endunless
+
+
+                <!-- using PHP directive to input some vanilla PHP inside -->
+                @php
+                    $name = 'Emmy';
+                    echo($name);
+                @endphp
+
            </div>
            
         </div>
