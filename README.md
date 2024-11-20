c1218df9770d9a7d2ded5f0494ad3b49a94992d2
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-11-21 00:19:50 +0100

    reusable layout files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 -   Blade is a Template engine which makes it eaasier to work with data and logic inside the view than regular PHP does.
+
+-   When a user types a URL and hits "Send," the web browser sends a request to the server hosting the website. The server passes this request to a route file, which examines the requested URL and determines the appropriate action. This action could involve processing data, saving it to a database, retrieving data, or injecting data into a view. The server then sends the response back to the user, allowing them to see the processed information in their web browser.
+
+-   When passind data to the view, we add a second argument to the VIEW function which the data we want to pass to the view.
--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,49 +1,38 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+@extends('layouts.layout')
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+@section('content')
+<div class="flex-center position-ref full-height">
 
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
+    <div class="content">
+        <h1 class="title m-b-md">
+            Pizza List
+        </h1>
 
-           <div class="content">
-                <h1 class="title m-b-md">
-                    Pizza List
-                </h1>
+        <!-- @for($i = 0; $i < 5; $i++)
+            <p>The value of i is {{ $i }}</p>
+        @endfor -->
+        
+        <!-- @for($i = 0; $i < count($pizzas); $i++)
+            <p>{{ $pizzas[$i]['type'] }}</p>
+        @endfor -->
 
-                <!-- @for($i = 0; $i < 5; $i++)
-                    <p>The value of i is {{ $i }}</p>
-                @endfor -->
+        @foreach($pizzas as $pizza)
+            <div> 
+
+                {{ $loop->index +1}}: {{ $pizza['type'] }} - {{ $pizza['base'] }} 
+
+                @if($loop->first)
+                    <span>first in the loop</span>
+                @endif
+
+                @if($loop->last)
+                    <span>last in the loop</span>
+                @endif
                 
-                <!-- @for($i = 0; $i < count($pizzas); $i++)
-                    <p>{{ $pizzas[$i]['type'] }}</p>
-                @endfor -->
-
-                @foreach($pizzas as $pizza)
-                    <div> 
-
-                        {{ $loop->index +1}}: {{ $pizza['type'] }} - {{ $pizza['base'] }} 
-
-                        @if($loop->first)
-                            <span>first in the loop</span>
-                        @endif
-
-                        @if($loop->last)
-                            <span>last in the loop</span>
-                        @endif
-                        
-                    </div>
-                @endforeach
-           </div>
-           
-        </div>
-    </body>
-</html>
+            </div>
+        @endforeach
+    </div>
+    
+</div>
+@endsection
